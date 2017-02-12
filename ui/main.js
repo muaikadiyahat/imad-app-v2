49cc9df79d8015999441681bb829e85dd9f593d1
murari meena <muaikadiyahat@users.noreply.github.com>
2017-02-12 19:32:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
 
+//Change the text of main-text
 var element = document.getElementById('main-text');
 
 element.innerHTML = "New Value";
+
+//move the image
+var img = document.getElementById('img');
+img.onClick = function(){
+    img.style.marginLeft = '100px';
+};
