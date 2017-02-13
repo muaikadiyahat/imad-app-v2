86461729330d8f1fa88c47f66512aab7aece6326
murari meena <muaikadiyahat@users.noreply.github.com>
2017-02-13 23:37:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML = "New Value";
 //move the image
 var img = document.getElementById('madi');
 img.onClick = function () {
-    img.style.marginLeft = '100px';
+    madi.style.marginLeft = '100px';
 };
