b2d79c7be6835dd806e18ab7c6e01c3966c6f9be
murari meena <muaikadiyahat@users.noreply.github.com>
2017-02-15 10:51:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,22 @@
-console.log('Loaded!');
+// counter code
 
-//Change the text of main-text
-var element = document.getElementById('main-text');
-
-element.innerHTML = "New Value";
-
-//move the image
-var img = document.getElementById('madi');
-img.onclick = function () {
-    img.style.marginLeft = '100px';
+var button = document.getElementById('counter');
+button.onclick = function(){
+    // create a request object
+    
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            // take some action
+            
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+            }
+        }
+    };
+    request.open('GET','http://muaikadiyahat.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
