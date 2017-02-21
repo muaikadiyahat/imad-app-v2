6fb310cab148e9342c4bf8386e5b11a04e227e5c
murari meena <muaikadiyahat@users.noreply.github.com>
2017-02-21 10:57:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,23 @@
 // counter code
 
 var button = document.getElementById('counter');
-var counter=0;
 button.onclick = function(){
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+    // create a request object
+    
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            // take some action
+            
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://muaikadiyahat.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
