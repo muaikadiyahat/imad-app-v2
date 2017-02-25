d8a8b82e9b1f0ca0d134622f34a1681750f56343
murari meena <muaikadiyahat@users.noreply.github.com>
2017-02-25 13:53:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function(){
 
 //submit name
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
+var nam = nameInput.value;
 
 var submit=document.getElementById('submit_btn');
 
@@ -38,5 +38,3 @@ for(var i=0;i<names.length;i++){
 }
 var ul=document.getElementById('namelist');
 ul.innerHTML = list;
-
-};
